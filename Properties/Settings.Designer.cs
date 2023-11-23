72e25e2d4ee8490ba45ad97b26f5809984123050
Rasmus Löberg <rasmus.loberg04@gmail.com>
2023-11-23 23:24:20 +0100

    I didn't fix the bug, I give up

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Server_Application.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool stay_logged_in {
             get {
                 return ((bool)(this["stay_logged_in"]));
